b60a24e23e2ff311cf286c3601aec2e851f3c2a1
Abdul Ghafoor <abdul.ghafoor@dubizzlelabs.com>
2024-11-11 00:19:14 +0500

    Find missing in array with KadanesMethod's method

--- src/geeksforgeeks/T01MaximumSubarraySum.js
+++ src/geeksforgeeks/T01MaximumSubarraySum.js
@@ -13,6 +13,26 @@ function MaximumSubarraySum(arr) {
   return res;
 }
 
-console.log(MaximumSubarraySum([2, 3, -8, 7, -1, 2, 3]));
-console.log(MaximumSubarraySum([-2, -4]));
-console.log(MaximumSubarraySum([5, 4, 1, 7, 8]));
+// console.log(MaximumSubarraySum([2, 3, -8, 7, -1, 2, 3])); // 11
+// console.log(MaximumSubarraySum([-2, -4]));
+// console.log(MaximumSubarraySum([5, 4, 1, 7, 8]));
+// console.log(MaximumSubarraySum([1, 2, 3, -2, 5]));
+
+function MaximumSubarraySumWithKadanesMethod(arr) {
+  let maxSoFor = arr[0];
+  let maxEndingHere = arr[0];
+  for (let j = 1; j < arr.length; j++) {
+    maxEndingHere = Math.max(arr[j], maxEndingHere + arr[j]);
+    maxSoFor = Math.max(maxSoFor, maxEndingHere);
+
+    console.log("maxEndingHere ===> ", maxEndingHere);
+    console.log("maxSoFor ===> ", maxSoFor);
+  }
+
+  return maxSoFor;
+}
+
+console.log(MaximumSubarraySumWithKadanesMethod([2, 3, -8, 7, -1, 2, 3])); // 11
+// console.log(MaximumSubarraySumWithKadanesMethod([-2, -4]));
+// console.log(MaximumSubarraySumWithKadanesMethod([5, 4, 1, 7, 8]));
+// console.log(MaximumSubarraySumWithKadanesMethod([1, 2, 3, -2, 5]));
